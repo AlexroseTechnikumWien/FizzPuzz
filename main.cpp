acf51e7e2f45300fbc5de103122d5490c5f27c88
AlexroseTechnikumWien <if24b135@technikum-wien.at>
2025-04-04 10:06:23 +0200

    fizzbuzz methode geschrieben

--- main.cpp
+++ main.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
 
+void fizzbuzz(int n);
+
 int main()
 {
+    std::cout << "Enter a number: ";
+    int n;
+    std::cin >> n;
 
+    fizzbuzz(n);
 
     return 0;
 }
 
 void fizzbuzz(int n)
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-
+        if(i % 3 == 0 && i % 5 == 0)
+        {
+            std::cout << "FizzBuzz" << std::endl;
+        }
+        else if(i % 3 == 0)
+        {
+            std::cout << "Fizz" << std::endl;
+        }
+        else if(i % 5 == 0)
+        {
+            std::cout << "Buzz" << std::endl;
+        }
+        else
+        {
+            std::cout << i << std::endl;
+        }
     }
 }
