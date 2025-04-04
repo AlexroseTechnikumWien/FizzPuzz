d275b73f856ef4778cb80f6701c16c9f03d391da
AlexroseTechnikumWien <if24b135@technikum-wien.at>
2025-04-04 10:24:56 +0200

    rekursive methode

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+void rekfizzbuzz(int n);
+
 void fizzbuzz(int n)
 {
     for(int i = 1; i <= n; i++)
@@ -22,10 +24,17 @@ int main()
 
 void rekfizzbuzz(int n, int i)
 {
+    int i =1;
     if(i > n) return;
     if(i % 3 == 0 && i % 5 == 0) std::cout << "FizzBuzz" << std::endl;
     else if(i % 3 == 0) std::cout << "Fizz" << std::endl;
     else if(i % 5 == 0) std::cout << "Buzz" << std::endl;
     else std::cout << i << std::endl;
-    rekfizzbuzz(n, i + 1);
+    i++;
+    rekfizzbuzz(n);
+}
+
+void fizzbuzzoneline(int n)
+{
+    for(int i = 1; i <= n; i++) i % 3 == 0 && i % 5 == 0 ? std::cout << "FizzBuzz" << std::endl :i % 3 == 0 ? std::cout << "Fizz" << std::endl :i % 5 == 0 ? std::cout << "Buzz" << std::endl :std::cout << i << std::endl;
 }
