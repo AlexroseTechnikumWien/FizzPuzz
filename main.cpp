7a6ff16b3893775e4cff799c18f25f9234b0d017
AlexroseTechnikumWien <if24b135@technikum-wien.at>
2025-04-04 10:35:27 +0200

    neue Funktion

--- main.cpp
+++ main.cpp
@@ -19,12 +19,8 @@ int main()
     std::cout << "Enter a number: ";                        //Nummer eingeben
     int n;                                                  //Nummer zum eingeben
     std::cin >> n;                                          //Eingabe der Nummer
-    fizzbuzz(n);                                            //Funktion aufrufen
-    std::cout << "Recursive FizzBuzz:" << std::endl;
-    rekfizzbuzz(n, n-(n-1));                                //Rekursive Funktion aufrufen
-    std::cout << "One line FizzBuzz:" << std::endl;
-    fizzbuzzoneline(n);                                     //Funktion aufrufen
-    std::cout << "End of program." << std::endl;            //Ende des Programms
+
+
     return 0;                                               //return
 }
 
@@ -41,6 +37,11 @@ void rekfizzbuzz(int n,int i)
 void fizzbuzzoneline(int n)
 {
     for(int i = 1; i <= n; i++) i % 3 == 0 && i % 5 == 0 ? std::cout << "FizzBuzz" << std::endl :i % 3 == 0 ? std::cout << "Fizz" << std::endl :i % 5 == 0 ? std::cout << "Buzz" << std::endl :std::cout << i << std::endl;
+}
+
+void mitextrafizz(int n)
+{
+
 }
 
 // Hello everybody, my name is Markiplier, and welcome back to Five Nights at Freddy's!
