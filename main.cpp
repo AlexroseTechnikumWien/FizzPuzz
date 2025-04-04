86317e5bddbfdcdb9dada1679e9d76c87be8f015
AlexroseTechnikumWien <if24b135@technikum-wien.at>
2025-04-04 10:42:48 +0200

    klammern in den ifs

--- main.cpp
+++ main.cpp
@@ -44,15 +44,25 @@ void mitextrafizz(int n)
     for(int i = 1; i <= n; i++)
     {
         if(i%9==0 && i%5==0)
+        {
             std::cout<<"FizzBuzz mit E X T R A F I Z Z" << std::endl;
+        }
         else if(i%3==0 && i%5==0)
+        {
             std::cout<<"FizzBuzz" << std::endl;
+        }
         else if(i%9==0)
+        {
             std::cout<<"E X T R A F I Z Z" << std::endl;
+        }
         else if(i%3==0)
+        {
             std::cout<<"Fizz" << std::endl;
+        }
         else if(i%5==0)
+        {
             std::cout<<"Buzz" << std::endl;
+        }
         else
             std::cout<<i<<std::endl;
     }
